b03061de183d2147f407f3372caf1ed13aed4e18
DionlenoPiata <contato.dionleno@gmail.com>
2023-11-17 10:40:05 -0300

    Adding get by id

--- Controllers/TodoController.cs
+++ Controllers/TodoController.cs
@@ -1,7 +1,5 @@
-using System.Collections.Generic;
 using System.Threading.Tasks;
 using MeuTodo.Data;
-using MeuTodo.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -13,12 +11,21 @@ namespace MeuTodo.Controllers
     {
         [HttpGet]
         [Route("todos")]
-        public async Task<IActionResult> Get(
+        public async Task<IActionResult> GetAsync(
             [FromServices] AppDbContext context
         )
         {
             var todos = await context.Todos.AsNoTracking().ToListAsync();
             return Ok(todos);
+        }
+        [HttpGet]
+        [Route("todos/{id}")]
+        public async Task<IActionResult> GetByIdAsync(
+           [FromServices] AppDbContext context, [FromRoute] int id
+       )
+        {
+            var todo = await context.Todos.AsNoTracking().FirstOrDefaultAsync(x => x.Id == id);
+            return todo == null ? NotFound() : Ok(todo);
         }
     }
 }
