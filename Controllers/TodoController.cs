8eec653b224da00f58e62fb6329456c5d8181042
DionlenoPiata <contato.dionleno@gmail.com>
2023-11-17 11:00:21 -0300

    Adding create todo

--- Controllers/TodoController.cs
+++ Controllers/TodoController.cs
@@ -1,5 +1,8 @@
+using System;
 using System.Threading.Tasks;
 using MeuTodo.Data;
+using MeuTodo.Models;
+using MeuTodo.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -20,12 +23,35 @@ namespace MeuTodo.Controllers
         }
         [HttpGet]
         [Route("todos/{id}")]
-        public async Task<IActionResult> GetByIdAsync(
-           [FromServices] AppDbContext context, [FromRoute] int id
-       )
+        public async Task<IActionResult> GetByIdAsync([FromServices] AppDbContext context, [FromRoute] int id)
         {
             var todo = await context.Todos.AsNoTracking().FirstOrDefaultAsync(x => x.Id == id);
             return todo == null ? NotFound() : Ok(todo);
+        }
+        [HttpPost("todos")]
+        public async Task<IActionResult> PostAsync([FromServices] AppDbContext context, [FromBody] CreateTodoViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+            var todo = new Todo
+            {
+                Date = DateTime.Now,
+                Done = false,
+                Title = model.Title
+            };
+            try
+            {
+                await context.Todos.AddAsync(todo);
+                await context.SaveChangesAsync();
+                return Created($"v1/todos/{todo.Id}", todo);
+            }
+            catch (Exception e)
+            {
+
+                return BadRequest();
+            }
         }
     }
 }
